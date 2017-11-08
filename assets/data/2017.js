3afff8f83bb23dd66b7c89def93c9bf8842b269d
Christopher Lew <chrislew@lewcentral.com>
2017-11-08 17:22:54 -0500

    added testing data and radio buttons to determine data type

---
+++ assets/data/2017.js
@@ -0,0 +1,90 @@
+const PG2016 = [
+  {
+    "name": "Lebron James",
+    "team": "CLE",
+    "PTS": 26.4,
+    "AST": 8.7,
+    "STL": 1.2,
+    "TRB": 8.6
+  },
+  {
+    "name": "Kristaps Porzingis",
+    "team": "NYK",
+    "PTS": 18.1,
+    "AST": 1.5,
+    "STL": .7,
+    "TRB": 7.2
+  },
+  {
+    "name": "Russel Westbrook",
+    "team": "OKC",
+    "PTS": 31.6,
+    "AST": 10.4,
+    "STL": 1.6,
+    "TRB": 10.7
+  },
+  {
+    "name": "James Harden",
+    "team": "HOU",
+    "PTS": 29.1,
+    "AST": 11.2,
+    "STL": 1.5,
+    "TRB": 8.1
+  },
+  {
+    "name": "Stephen Currey",
+    "team": "GSW",
+    "PTS": 25.3,
+    "AST": 6.6,
+    "STL": 1.8,
+    "TRB": 4.5
+  },
+  {
+    "name": "Anthony Davis",
+    "team": "NOP",
+    "PTS": 28.0,
+    "AST": 2.1,
+    "STL": 1.3,
+    "TRB": 11.8
+  },
+  {
+    "name": "Kevin Durant",
+    "team": "GSW",
+    "PTS": 25.1,
+    "AST": 4.8,
+    "STL": 1.1,
+    "TRB": 8.3
+  },
+  {
+    "name": "John Wall",
+    "team": "WAS",
+    "PTS": 23.1,
+    "AST": 10.7,
+    "STL": 2.0,
+    "TRB": 4.2
+  },
+  {
+    "name": "Isaiah Thomas",
+    "team": "BOS",
+    "PTS": 28.9,
+    "AST": 5.9,
+    "STL": .9,
+    "TRB": 2.7
+  },
+  {
+    "name": "Carmelo Anthony",
+    "team": "NYK",
+    "PTS": 22.4,
+    "AST": 2.9,
+    "STL": .8,
+    "TRB": 5.9
+  },
+  {
+    "name": "Giannis Antetokounmpo",
+    "team": "MIL",
+    "PTS": 22.9,
+    "AST": 5.4,
+    "STL": 1.6,
+    "TRB": 8.8
+  },
+];
