49777cacf26bccd8e18813a14fe49c7cc2084922
Christopher Lew <chrislew@lewcentral.com>
2017-11-09 19:05:02 -0500

    plot working for set values

--- bundle.js
+++ bundle.js
@@ -9303,7 +9303,7 @@ function transform(node) {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__assets_data_PG2016_json__ = __webpack_require__(466);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__assets_data_PG2016_json__ = __webpack_require__(464);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__assets_data_PG2016_json___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__assets_data_PG2016_json__);
 const d3 = __webpack_require__(172);
 const $ = __webpack_require__(463);
@@ -33214,9 +33214,7 @@ return jQuery;
 
 
 /***/ }),
-/* 464 */,
-/* 465 */,
-/* 466 */
+/* 464 */
 /***/ (function(module, exports) {
 
 module.exports = [{"name":"Lebron James","team":"CLE","PTS":26.4,"AST":8.7,"STL":1.2,"TRB":8.6},{"name":"Kristaps Porzingis","team":"NYK","PTS":18.1,"AST":1.5,"STL":0.7,"TRB":7.2},{"name":"Russel Westbrook","team":"OKC","PTS":31.6,"AST":10.4,"STL":1.6,"TRB":10.7},{"name":"James Harden","team":"HOU","PTS":29.1,"AST":11.2,"STL":1.5,"TRB":8.1},{"name":"Stephen Currey","team":"GSW","PTS":25.3,"AST":6.6,"STL":1.8,"TRB":4.5},{"name":"Anthony Davis","team":"NOP","PTS":28,"AST":2.1,"STL":1.3,"TRB":11.8},{"name":"Kevin Durant","team":"GSW","PTS":25.1,"AST":4.8,"STL":1.1,"TRB":8.3},{"name":"John Wall","team":"WAS","PTS":23.1,"AST":10.7,"STL":2,"TRB":4.2},{"name":"Isaiah Thomas","team":"BOS","PTS":28.9,"AST":5.9,"STL":0.9,"TRB":2.7},{"name":"Carmelo Anthony","team":"NYK","PTS":22.4,"AST":2.9,"STL":0.8,"TRB":5.9},{"name":"Giannis Antetokounmpo","team":"MIL","PTS":22.9,"AST":5.4,"STL":1.6,"TRB":8.8}]
