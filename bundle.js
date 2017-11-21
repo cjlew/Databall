85346a2c889c726c0627761da6a097361e96ea0a
Christopher Lew <chrislew@lewcentral.com>
2017-11-21 16:57:40 -0500

    fixed json path

--- bundle.js
+++ bundle.js
@@ -23032,9 +23032,9 @@ const makePlot = (Params) => {
       .append("g")
         .attr("transform","translate(" + margin.left + "," + margin.top + ")");
   //Local
-  d3.json(`../assets/data/${Params.type}${Params.Year}.json`, (error, data) => {
+  // d3.json(`../assets/data/${Params.type}${Params.Year}.json`, (error, data) => {
   //Production
-  // d3.json(`/Databall/assets/data/${Params.type}${Params.Year}.json`, (error, data) => {
+  d3.json(`/Databall/assets/data/${Params.type}${Params.Year}.json`, (error, data) => {
     if (error) throw error;
 
     const refinedData = Object(__WEBPACK_IMPORTED_MODULE_0__parametrize_js__["a" /* parametrize */])(data, Params);
